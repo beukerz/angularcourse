d9a63eb25d8edf0266226c4adf7bd0f378107321
Hugo Beukers <hugo.beukers@vattenfall.com>
2022-03-30 08:57:35 +0200

    adding all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { InformationComponent } from './information/information.component';
+import { WarningComponent } from './warning/warning.component'
+import { AlertsComponent } from './alerts/alerts.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    InformationComponent,
+    WarningComponent,
+    AlertsComponent
   ],
   imports: [
     BrowserModule
